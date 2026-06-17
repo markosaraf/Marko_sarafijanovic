db3038b79cbdca67a136c69c4c14515c44650f04
Marko Sarafijanovic <118335343+markosaraf@users.noreply.github.com>
2026-06-17 20:07:08 +0200

    Update fsd-page.tsx

--- src/app/Why-allow-Tesla-FSD-Supervised/fsd-page.tsx
+++ src/app/Why-allow-Tesla-FSD-Supervised/fsd-page.tsx
@@ -32,8 +32,8 @@ const tweetTitles: Record<string, string> = {
   '1967307929444348158': 'Prevent deaths',
   '2024470956811374598': 'Prevent accidents and injuries',
   '2065524907551735922': 'Stop state favorism and regulatory capture in Switzerland.🇨🇭',
-  '2065459025186025718': 'Revert Article 34c of VAF (Verordnung über das automatisierte Fahren). Article 34c is leading to state favorism by ASTRA. ASTRA is excempting state projects like "AmiGo" by PostAuto AG from Article 34c.🇨🇭 Current ASTRA Director: Jürg Röthlisberger. ',
-  '2067300358464934034': 'Tesla FSD (Supervised) is statistically safer than the 100% state owned PostAuto AG fleet.',
+  '2065459025186025718': 'Revert Article 34c of VAF (Verordnung über das automatisierte Fahren). Article 34c is leading to state favorism by ASTRA. ASTRA is excempting state projects like "AmiGo" by PostAuto AG from article 34c.🇨🇭 Current ASTRA Director: Jürg Röthlisberger. ',
+  '2067300358464934034': 'Tesla FSD (Supervised) is statistically safer than the 100% state owned PostAuto AG fleet. Yet ASTRA gave PostAuto AG an excemption from article 34c of VAF. That's the article preventing Tesla FSD (Supervised) in Switzerland. ',
 };
 
 // Thread replies for the first tweet
