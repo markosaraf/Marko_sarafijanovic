9a6a21dafe12cea2f5116a5be74f8630d7483687
Marko Sarafijanovic <118335343+markosaraf@users.noreply.github.com>
2026-06-18 17:57:00 +0200

    Update page.tsx

--- src/app/Why-allow-Tesla-FSD-Supervised/page.tsx
+++ src/app/Why-allow-Tesla-FSD-Supervised/page.tsx
@@ -3,7 +3,7 @@ import FSDPage from "./fsd-page";
 
 // SEO Metadata for Google Search Results
 export const metadata: Metadata = {
-  title: "Marko Sarafijanovic: Why allow Tesla FSD (Supervised) in Switzerland?",
+  title: "Marko Sarafijanovic: Why approve Tesla FSD (Supervised) in Switzerland?",
   description: "Reasons to allow Tesla FSD in Switzerland: Prevent traffic deaths and traffic accidents, increase traffic safety, Stop state favorism and regulatory capture in Switzerland.",
   keywords: ["Tesla FSD", "Full Self-Driving", "Switzerland", "Traffic Safety", "Autonomous Driving", "Tesla", "FSD Supervised", "Traffic Deaths Prevention"],
   authors: [{ name: "Marko Sarafijanovic" }],
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     canonical: "https://marko-sarafijanovic.com/Why-allow-Tesla-FSD-Supervised",
   },
   openGraph: {
-    title: "Why allow Tesla FSD in Switzerland? — Marko Sarafijanovic",
+    title: "— Marko Sarafijanovic: Why approve Tesla FSD (Supervised) in Switzerland?",
     description: "Reasons to allow Tesla FSD in Switzerland: Prevent traffic deaths and traffic accidents, increase traffic safety, Stop state favorism and regulatory capture in Switzerland.",
     url: "https://marko-sarafijanovic.com/Why-allow-Tesla-FSD-Supervised",
     siteName: "Marko Sarafijanovic",
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "Why allow Tesla FSD in Switzerland? — Marko Sarafijanovic",
+    title: "— Marko Sarafijanovic: Why approve Tesla FSD (Supervised) in Switzerland?",
     description: "Reasons to allow Tesla FSD in Switzerland: Prevent traffic deaths and traffic accidents, increase traffic safety, Stop state favorism and regulatory capture in Switzerland.",
     creator: "@MarkoSaraf2004",
     images: ["/marko-profile.png"],
