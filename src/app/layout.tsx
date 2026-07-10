7f17ef0dd616c47153ab18166355b45cea52838c
Marko Sarafijanovic <118335343+markosaraf@users.noreply.github.com>
2026-07-10 13:54:46 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
@@ -74,6 +75,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
